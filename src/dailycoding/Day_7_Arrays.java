c74cd924bbb1b5994766fe337bf61e078edb5db8
adnan.isajbegovic <adnan.isajbegovic@hpe.com>
2016-11-01 11:03:03 +0100

    Day 7
    Added Day 7: Arrays

---
+++ src/dailycoding/Day_7_Arrays.java
@@ -0,0 +1,21 @@
+package dailycoding;
+
+import java.util.Scanner;
+
+/**
+ * @Author: Adnan Isajbegovic
+ * @Created: 2016-11-01
+ */
+public class Day_7_Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        in.close();
+
+        while(n > 0) System.out.print(arr[--n] + " ");
+    }
+}
