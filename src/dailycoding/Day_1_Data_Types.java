647326d7a16df0f1ce6ed5b64409f116c640a6c7
adnan.isajbegovic <adnan.isajbegovic@hpe.com>
2016-11-01 10:29:15 +0100

    Day 1
    Added solution for Day 1: Data Types

---
+++ src/dailycoding/Day_1_Data_Types.java
@@ -0,0 +1,26 @@
+package dailycoding;
+
+import java.util.Scanner;
+
+/**
+ * @Author: Adnan Isajbegovic
+ * @Created: 2016-11-01
+ */
+public class Day_1_Data_Types {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        Scanner scan = new Scanner(System.in);
+
+        int secondInteger = scan.nextInt();
+        double secondDouble = scan.nextDouble();
+        scan.nextLine();    // move to new line
+        String secondString = scan.nextLine();
+        System.out.println(i + secondInteger);
+        System.out.println(d + secondDouble);
+        System.out.println(s + secondString);
+
+        scan.close();
+    }
+}
